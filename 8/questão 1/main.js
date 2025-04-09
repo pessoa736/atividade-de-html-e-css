d65ce4681adb380dbc28ca6347aafd7624b39414
Davi dos Santos Passos <69681778+pessoa736@users.noreply.github.com>
2025-04-09 20:45:01 +0000

    skibidi toilet

---
+++ 8/questão 1/main.js
@@ -0,0 +1,11 @@
+
+const input = (str) => { return parseInt(window.prompt(str, 0)) }
+
+var n1, n2
+
+    n1 = input("diga um numero ai!")
+
+    n2 = input("me diz outro, senpai-kun 👉👈🥺 =uwu= yamete")
+
+
+alert(`soma: \t ${n1 + n2} \n subtração: \t ${n1 - n2} \n mutiplicação: \t ${n1*n2} \n divisão: \t ${n1/n2}    `)
